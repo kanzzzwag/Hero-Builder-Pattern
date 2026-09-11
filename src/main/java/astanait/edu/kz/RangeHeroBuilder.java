1ef1bbf338a227a14ad4b355a3e9fb16adcf117d
Batyrkhan <batyrkhankairatuly1@gmail.com>
2026-09-11 14:54:11 +0500

    recreated the RangeHeroBuilder class

--- src/main/java/astanait/edu/kz/RangeHeroBuilder.java
+++ src/main/java/astanait/edu/kz/RangeHeroBuilder.java
@@ -3,6 +3,13 @@ package astanait.edu.kz;
 public class RangeHeroBuilder extends AbstractHeroBuilder{
 
     public RangeHeroBuilder(){
-        this.attackRange =
+        this.weaponType = WeaponType.RANGED;
+    }
+
+    public RangeHeroBuilder setAttackRange(AttackRange range){
+        if(range == AttackRange.CLOSE){
+            throw new IllegalStateException("Ranged hero's attack range can't be close");}
+        this.attackRange = attackRange;
+        return this;
     }
 }
